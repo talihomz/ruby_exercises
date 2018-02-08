b72a85b5b55931d28f7e99038c07e7e0a57c0d37
Sava <vuckovicsava11@gmail.com>
2018-02-08 09:05:12 +0100

    bubble_sort_by created

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,4 +19,23 @@ def bubble_sort(array)
   return array
 end
 
-p bubble_sort [1,2,8,3, 23,-7, 5, 345]
+
+def bubble_sort_by(array)
+  stop = false
+
+  until stop do
+    stop = true
+    for a in (0...array.length - 1)
+
+      res = yield(array[a], array[a + 1])
+      if res == 1
+        array[a], array[a + 1] = array[a + 1], array[a]
+        stop = false
+      end
+    end
+  end
+
+  return array
+end
+
+p bubble_sort_by(["one", "three", "seventy", "a", "aaaaaaaaaaaaaaaaaa"]) { |a, b| b.length <=> a.length }
